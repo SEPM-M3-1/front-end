1e2a4d7cdd4316d5b52bf9224564547133597ca5
Robert Xu <kritoxu@gmail.com>
2021-05-06 17:49:09 +1000

    add folder and set file

---
+++ src/components/DashBoardM/components/AddShift/AddShift.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const AddShift = () => {
+
+}
+
+export default AddShift;
---
+++ src/components/DashBoardM/components/ProfileM/ProfileM.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ProfileM = () => {
+
+}
+
+export default ProfileM;
